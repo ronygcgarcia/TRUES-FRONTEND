18119f7520bab815844ac6930b0d52f6dee263d9
noe <publicponce@outlook.com>
2021-07-03 18:01:25 -0600

    create user

--- src/layouts/Users/Users.js
+++ src/layouts/Users/Users.js
@@ -9,19 +9,76 @@ import {
   TableBody,
   TableRow,
   Button,
+  Modal,
+  TextField,
 } from "@material-ui/core";
 import { Edit, Delete } from "@material-ui/icons";
 import "./User.css";
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  iconos: {
+    cursor: "pointer",
+  },
+  inputMaterial: {
+    width: "100%",
+  },
+}));
 
-
+//PRINCIPAL
 function Users() {
   const [users, setUsers] = useState([]);
+  const styles = useStyles();
+
+
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
+  //const [modalEliminar, setModalEliminar]=useState(false);
+
+  const [userSelected, setUserSelected] = useState({
+    name: "",
+    uid: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserSelected((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(userSelected);
+  };
+
+  /*const handleChangeCreate=e=>{
+
+  }*/
+
+  const abrirCerrarModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
+  };
+
+  const abrirCerrarModalEditar = () => {
+    setModalEditar(!modalEditar);
+  };
+
+  
 
   const getUsers = async () => {
     try {
       const resp = await api.get("/users");
-      console.log(resp.data);
       setUsers(resp.data);
     } catch (err) {
       // Handle Error Here
@@ -29,14 +86,128 @@ function Users() {
     }
   };
 
+  const createUser = async () => {
+    try {
+      const resp = await api.post("/users",userSelected).then((response) => {
+        setUsers(users.concat(response.data));
+        console.log(users);
+        console.log(response.data);
+        abrirCerrarModalInsertar();
+      });
+    } catch (error) {
+      abrirCerrarModalInsertar();
+      console.error(error);
+    }
+  };
+
+  const updateUser = async () => {
+    try {
+      const resp = await api.post("/users", userSelected).then((response) => {
+        setUsers(users.concat(response.data));
+        abrirCerrarModalInsertar();
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
 
+  const bodyInsertar = (
+    <div className={styles.modal}>
+      <h3>Agregar Nuevo Usuario</h3>
+      <TextField
+        name="name"
+        className={styles.inputMaterial}
+        label="Nombre"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        name="uid"
+        className={styles.inputMaterial}
+        label="Nombre de Usuario"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        name="email"
+        className={styles.inputMaterial}
+        label="Correo Electronico"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        type="password"
+        name="password"
+        className={styles.inputMaterial}
+        label="Contraseña"
+        onChange={handleChange}
+      />
+      <br />
+      <TextField
+        type="password"
+        name="password_confirmation"
+        className={styles.inputMaterial}
+        label="Verificar Contraseña"
+        onChange={handleChange}
+      />
+      <br />
+      <br />
+      <br />
+      <div align="right">
+        <Button color="primary" onClick={() => createUser()}>
+          Insertar
+        </Button>
+        <Button onClick={() => abrirCerrarModalInsertar()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
+  const bodyEditar = (
+    <div className={styles.modal}>
+      <h3>Editar Usuario</h3>
+      <TextField
+        name="name"
+        className={styles.inputMaterial}
+        label="Nombre"
+        onChange={handleChange}
+        value={userSelected && userSelected.name}
+      />
+      <br />
+      <TextField
+        name="username"
+        className={styles.inputMaterial}
+        label="Nombre de Usuario"
+        onChange={handleChange}
+        value={userSelected && userSelected.username}
+      />
+      <br />
+      <TextField
+        name="email"
+        className={styles.inputMaterial}
+        label="Correo Electronico"
+        onChange={handleChange}
+        value={userSelected && userSelected.email}
+      />
+      <br />
+      <br />
+      <br />
+      <div align="right">
+        <Button color="primary" onClick={() => updateUser()}>
+          Editar
+        </Button>
+        <Button onClick={() => abrirCerrarModalEditar()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="users__crud">
       <br />
-      <Button>Insertar</Button>
+      <Button onClick={() => abrirCerrarModalInsertar()}>Insertar</Button>
       <br />
       <br />
       <TableContainer>
@@ -46,7 +217,7 @@ function Users() {
               <TableCell>Nombre</TableCell>
               <TableCell>Username</TableCell>
               <TableCell>Correo</TableCell>
-              <TableCell>Roles</TableCell>
+
               <TableCell>Acciones</TableCell>
             </TableRow>
           </TableHead>
@@ -54,18 +225,9 @@ function Users() {
           <TableBody>
             {users.map((user) => (
               <TableRow key={user.id}>
-                {console.log(user)}
                 <TableCell>{user.name}</TableCell>
                 <TableCell>{user.username}</TableCell>
                 <TableCell>{user.email}</TableCell>
-                <TableCell>
-                  {user.role.map((rol) => (
-                    <li key={rol.id}>
-                      {rol.name}
-                      <br/>
-                    </li>
-                  ))}
-                </TableCell>
                 <TableCell>
                   <Edit />
                   <Delete />
@@ -75,6 +237,13 @@ function Users() {
           </TableBody>
         </Table>
       </TableContainer>
+
+      <Modal open={modalInsertar} onClose={abrirCerrarModalInsertar}>
+        {bodyInsertar}
+      </Modal>
+      <Modal open={modalEditar} onClose={abrirCerrarModalEditar}>
+        {bodyEditar}
+      </Modal>
     </div>
   );
 }
