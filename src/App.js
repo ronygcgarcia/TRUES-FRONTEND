6f3838c4d8c67deb7260f11878a5222e7406b8d7
noe <publicponce@outlook.com>
2021-07-15 20:21:38 -0600

    drawer login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Login from './layouts/Login/Login';
 import Routes from './routes/Routes';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => 
+(  
+  <Route {...rest} render={props => 
+  (
+    localStorage.getItem('token') ? <Component {...props} /> : <Redirect to={{pathname: '/login'}}/>
+  )}/>
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <Router>
+    <Switch>
+      <PrivateRoute path="/" component={Routes} exact />
+
+      <Route path="/login" component={Login} />
+    </Switch>      
+  </Router>
   );
 }
 
