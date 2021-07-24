1cd616dd49af37d2c8a843123b9a22c27614b513
noe <publicponce@outlook.com>
2021-07-23 23:47:47 -0600

    Login redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React, {useState} from 'react';
 import PersistentDrawerLeft from './components/Drawer/Drawer';
+import Login from './layouts/Login/Login';
 
 function App() {
+  const [conectado, setconectado] = useState(false);   
+  const acceder=(estado)=>{
+    setconectado(estado);
+    console.log(estado)
+  }
   return (
-    <div className="App">
-      <PersistentDrawerLeft />
-    </div>
+    conectado ? <PersistentDrawerLeft/> : <Login acceder={acceder}/>
   );
 }
 
--- src/layouts/Login/Login.js
+++ src/layouts/Login/Login.js
@@ -1,99 +1,118 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import logo from "../../assets/ues_logo3.svg";
-import './login.css';
-import api from '../../config/axios';
+import "./login.css";
+import api from "../../config/axios";
 const Login = (props) => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        username: '',
-        password: ''
-    })
+  const { username, password } = user;
 
-
-    const { username, password } = user;
-
-
-    const login = async datos => {
-        try {
-
-            const credenciales = {
-                username: datos.username[0],
-                password: datos.password[0]
-            }
-            api.post('/login', credenciales)
-                .then(response => {
-                    localStorage.setItem('token', response.data.jwt)
-                    api.get('/user').then(response=>{
-                        console.log(response)
-                    })                    
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const handleChange = e => {
-        setUser({
-            ...user,
-            [e.target.name]: [e.target.value]
+  const login = async (datos) => {
+    try {
+      const credenciales = {
+        username: datos.username[0],
+        password: datos.password[0],
+      };
+      api
+        .post("/login", credenciales)
+        .then((response) => {
+          localStorage.setItem("token", response.data.jwt);
+          const respuesta =  api.get("/user").then((response) => {
+            console.log(response);
+          });
+          props.acceder(respuesta)
         })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    //inicio de sesion
-    const onSubmit = e => {
-        e.preventDefault();
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: [e.target.value],
+    });
+  };
 
-        //validar que no hayan campos vacios 
+  //inicio de sesion
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        //pasarlo al action 
-        login({ username, password });
-    }
+    //validar que no hayan campos vacios
 
-    return (
-        <div className='container__login'>
-            <div className="container__logo">
-                <img src={logo} alt="Universidad de El Salvador" />
-                <div className="logo__text">
-                    <strong>Universidad de El Salvador</strong>
-                    <p>Sistema de Tramites Universidad de El Salvador</p>
-                    <p>TRUES</p>
-                </div>
-            </div>
-            <div className="login__card">
-                <div className="container card bg-white rounded-lg mr-28 ml-28">
-                    <div className="card-header h-12 bg-red-700 rounded-t-lg flex items-center justify-center font-bold text-xl">
-                        <label className="block text-white" htmlFor="username">
-                            Iniciar sesion
-                        </label>
-                    </div>
-                    <div className="card-body">
-                        <form onSubmit={onSubmit}>
-                            <div className="mb-4 mt-4 mr-16 ml-16 row">
-                                <label className="block text-gray-700 text-sm font-bold mb-2 text-base" htmlFor="username">
-                                    Usuario
-                                </label>
-                                <input onChange={handleChange} id="username" name="username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Usuario" />
-                            </div>
-                            <div className="mb-4 mt-4 mr-16 ml-16 row">
-                                <label className="block text-gray-700 text-sm font-bold mb-2 text-base" htmlFor="password">
-                                    Contraseña
-                                </label>
-                                <input onChange={handleChange} id="password" name="password" type="password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Contraseña" />
-                            </div>
-                            <div className="mb-4 mt-4 mr-4 ml-4 row">
-                                <button type="submit" className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 mr-4 px-4 rounded">
-                                    Iniciar sesion
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+    //pasarlo al action
+    login({ username, password });
+  };
+
+  return (
+    <div className="container__login">
+      <div className="container__logo">
+        <img src={logo} alt="Universidad de El Salvador" />
+        <div className="logo__text">
+          <strong>Universidad de El Salvador</strong>
+          <p>Sistema de Tramites Universidad de El Salvador</p>
+          <p>TRUES</p>
         </div>
-    )
-}
+      </div>
+      <div className="login__card">
+        <div className="container card bg-white rounded-lg mr-28 ml-28">
+          <div className="card-header h-12 bg-red-700 rounded-t-lg flex items-center justify-center font-bold text-xl">
+            <label className="block text-white" htmlFor="username">
+              Iniciar sesion
+            </label>
+          </div>
+          <div className="card-body">
+            <form onSubmit={onSubmit}>
+              <div className="mb-4 mt-4 mr-16 ml-16 row">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2 text-base"
+                  htmlFor="username"
+                >
+                  Usuario
+                </label>
+                <input
+                  onChange={handleChange}
+                  id="username"
+                  name="username"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Usuario"
+                />
+              </div>
+              <div className="mb-4 mt-4 mr-16 ml-16 row">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2 text-base"
+                  htmlFor="password"
+                >
+                  Contraseña
+                </label>
+                <input
+                  onChange={handleChange}
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Contraseña"
+                />
+              </div>
+              <div className="mb-4 mt-4 mr-4 ml-4 row">
+                <button
+                  type="submit"
+                  className="bg-red-700 hover:bg-red-500 text-white font-bold py-2 mr-4 px-4 rounded"
+                >
+                  Iniciar sesion
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
