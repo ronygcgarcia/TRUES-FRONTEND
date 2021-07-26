8eee691ff29290a198e2219aa7e9099e76b9fffb
noe <publicponce@outlook.com>
2021-07-25 21:45:25 -0600

    : )

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 
 const Home = ({usuario}) => {
-    console.log(usuario);
     return (
-        <div>  
-            {usuario.permissions.find(permiso => permiso.name==='ver personal') ? <p>Encontrado</p> : <p>No encontrado</p>}
-           
+        <div>
+           Hola {usuario.name} !
         </div>
     )
 }
