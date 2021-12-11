cf8ff80366f89daaacbae86d25f4fc454ba3b512
noe <publicponce@outlook.com>
2021-12-11 01:44:09 -0600

    Agregando algunos estilos

--- src/layouts/UsuarioTramite/UsuarioTramite.js
+++ src/layouts/UsuarioTramite/UsuarioTramite.js
@@ -5,7 +5,7 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
@@ -13,9 +13,13 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Switch from "@material-ui/core/Switch";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import AddBoxIcon from "@material-ui/icons/AddBox";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
@@ -27,11 +31,11 @@ import api from "../../config/axios";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    height: "100%",
   },
   content: {
     flexGrow: 1,
-    height: "100vh",
-    overflow: "auto",
+    height: "100%",
   },
   container: {
     paddingTop: theme.spacing(4),
@@ -54,7 +58,7 @@ const useStyles = makeStyles((theme) => ({
   },
   modal: {
     position: "absolute",
-    width: 400,
+    width: 500,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
@@ -64,31 +68,40 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "8px",
   },
   selectTramite: {
-    minWidth: 200,
+    margin: theme.spacing(1),
+    minidth: 300,
+    height: "100%",
+  },
+  pasos: {
+    width: "40%",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    
+  },
+  formButtons: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    
   },
 }));
 
-const tramitesGet = [];
-
-api.get("/tramite").then((response) => {
-  response.data.forEach(function (element) {
-    tramitesGet.push(element);
-  });
-});
-
 function UsuarioTramite({ usuarioLog }) {
   const classes = useStyles();
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  const fixedHeightPaper = clsx(classes.paper);
   const [requestError, setRequestError] = useState();
   const [usuario, setUsuario] = useState({
     uid: "",
   });
   const [tramites, setTramites] = useState([]);
+  const [tramitesGet, setTramitesGet] = useState([]);
   const [estadoModal, setEstadoModal] = useState(false);
   const [tramiteAsignar, setTramiteAsignar] = React.useState(
     "Seleccione un tramite para asignar"
   );
   const [pasos, setPasos] = useState([]);
+  const [checked, setChecked] = React.useState([]);
 
   const handleModal = () => {
     setEstadoModal(!estadoModal);
@@ -109,9 +122,38 @@ function UsuarioTramite({ usuarioLog }) {
     event.preventDefault();
   };
 
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    console.log(value);
+    setChecked(newChecked);
+  };
+
+  const getTramites = async () => {
+    const tramitesAr = [];
+    try {
+      api.get("/tramite").then((response) => {
+        response.data.forEach(function (element) {
+          tramitesAr.push(element);
+        });
+      });
+      setTramitesGet(tramitesAr);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const buscarUsuario = async () => {
     if (usuario.uid !== "") {
+      console.log(usuario.uid);
       try {
+        console.log(usuario.uid);
         await api.get("/tramites/" + usuario.uid).then((response) => {
           setTramites(response.data.user.tramites);
           setUsuario(response.data.user);
@@ -165,13 +207,19 @@ function UsuarioTramite({ usuarioLog }) {
     }
   };
 
+  useEffect(() => {
+    getTramites();
+  }, []);
+
   const modalAsignacion = (
     <div>
       <form className={classes.modal} onSubmit={handleSubmit}>
         <FormGroup>
+        <Typography variant="h4">Asignar Tramite a Usuario</Typography>
+        &nbsp;
           <FormControl className={classes.formControl}>
-            <InputLabel className={classes.selectTramite}>
-              Selecciones un Tramite para Asignar
+            <InputLabel >
+              Tramites Disponibles
             </InputLabel>
             <Select
               labelId="demo-simple-select-label"
@@ -188,16 +236,17 @@ function UsuarioTramite({ usuarioLog }) {
               ))}
             </Select>
           </FormControl>
-          <FormControl>
-            <Button variant="contained" color="secondary" onClick={handleModal}>
-              Cerrar
-            </Button>
+          <FormControl className={classes.formButtons}>
             <Button
               variant="contained"
               color="primary"
               onClick={() => agregarTramite()}
-            >
+              >
               Agregar
+            </Button>
+              &nbsp;&nbsp;
+            <Button variant="contained" color="secondary" onClick={handleModal}>
+              Cerrar
             </Button>
           </FormControl>
         </FormGroup>
@@ -282,6 +331,7 @@ function UsuarioTramite({ usuarioLog }) {
                     <ListItem
                       button
                       key={index}
+                      divider
                       onClick={() => {
                         showPasos(tramite.id);
                       }}
@@ -294,6 +344,13 @@ function UsuarioTramite({ usuarioLog }) {
                         )}
                       </ListItemIcon>
                       <ListItemText primary={tramite.nombre} />
+                      <ListItemSecondaryAction>
+                        <IconButton
+                        edge="end"
+                        aria-label="delete">
+                          <DeleteIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
                     </ListItem>
                   );
                 })}
@@ -301,7 +358,7 @@ function UsuarioTramite({ usuarioLog }) {
             </Paper>
           </Grid>
           {/* Recent Orders */}
-          <Grid item xs={12}>
+          <Grid item xs={12} className={classes.pasos}>
             <Paper className={classes.paper}>
               <Typography
                 component="h2"
@@ -314,13 +371,25 @@ function UsuarioTramite({ usuarioLog }) {
               <List component="nav" aria-label="main mailbox folders">
                 {pasos.map((paso, index) => {
                   return (
-                    <ListItem button key={index}>
+                    <ListItem button key={index} divider>
                       <ListItemIcon>
                         <AssignmentIcon />
                       </ListItemIcon>
                       <ListItemText
                         primary={paso[0].tramite_paso.pasos.nombre}
                       />
+                      <ListItemSecondaryAction>
+                        <Switch
+                          edge="end"
+                          onChange={handleToggle(paso[0].tramite_paso_id)}
+                          checked={
+                            checked.indexOf(paso[0].tramite_paso_id) !== -1
+                          }
+                          inputProps={{
+                            "aria-labelledby": "switch-list-label-wifi",
+                          }}
+                        />
+                      </ListItemSecondaryAction>
                     </ListItem>
                   );
                 })}
